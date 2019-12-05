650b4a16f762b082e9c4191096239134f563a17d
Kieran Vella <kvella@ness.com.au>
2019-12-05 16:19:54 +1100

    Create nessvms.sh

---
+++ nessvms.sh
@@ -0,0 +1,14 @@
+#Ness VMS Server Setup Script
+#Update Server
+sudo apt update && apt upgrade
+#Grab some dependencies
+apt install wget
+apt install gdebi
+#Download the latest Nx Server Release
+wget "http://updates.networkoptix.com/default/29987/linux/nxwitness-server-4.0.0.29987-linux64.deb" /Downloads
+#Download the latest Nx Desktop Client Release
+wget "http://updates.networkoptix.com/default/29987/linux/nxwitness-client-4.0.0.29987-linux64.deb"
+#Install NX Server
+sudo gdebi nxwitness-server-4.0.0.29987-linux64.deb
+#Install Nx Client
+sudo gdebi nxwitness-client-4.0.0.29987-linux64.deb
