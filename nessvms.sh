b32431fe2d0dd7264cdf3bc6d85ae9c15aaf7c75
Kieran Vella <kvella@ness.com.au>
2019-12-06 15:17:52 +1100

    Update nessvms.sh

--- nessvms.sh
+++ nessvms.sh
@@ -1,14 +1,29 @@
 #Ness VMS Server Setup Script
-#Update Server
-sudo apt update && apt upgrade
 #Grab some dependencies
-sudo apt install wget
-sudo apt install gdebi
+sudo apt -y install figlet beep wget gdebi cockpit
+#Remove Amazon Crap
+echo "Remove Amazon Stuff" | figlet
+sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/Amazon.user.js
+sudo rm /usr/share/unity-webapps/userscripts/unity-webapps-amazon/manifest.json
+#Remove Extra stuff
+echo "Remove Other Apps" | figlet
+sudo apt -y purge libreoffice* thunderbird
+sudo apt -y clean
+sudo apt -y autoremove
+#Update Server
+echo "Update Server" | figlet
+sudo apt -y update
+sudo apt -y upgrade
 #Download the latest Nx Server Release
-wget "http://updates.networkoptix.com/default/29987/linux/nxwitness-server-4.0.0.29987-linux64.deb" /Downloads
+echo "Download NxWitness" | figlet
+wget "http://updates.networkoptix.com/default/29987/linux/nxwitness-server-4.0.0.29987-linux64.deb" -P ~/Downloads
 #Download the latest Nx Desktop Client Release
-wget "http://updates.networkoptix.com/default/29987/linux/nxwitness-client-4.0.0.29987-linux64.deb"
+wget "http://updates.networkoptix.com/default/29987/linux/nxwitness-client-4.0.0.29987-linux64.deb" -P ~/Downloads
 #Install NX Server
-sudo gdebi nxwitness-server-4.0.0.29987-linux64.deb
+echo "Install NxWitness" | figlet
+sudo gdebi --non-interactive ~/Downloads/nxwitness-server-4.0.0.29987-linux64.deb
 #Install Nx Client
-sudo gdebi nxwitness-client-4.0.0.29987-linux64.deb
+sudo gdebi --non-interactive ~/Downloads/nxwitness-client-4.0.0.29987-linux64.deb
+#Finished!
+echo "All Done!" | figlet
