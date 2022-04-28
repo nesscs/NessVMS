ddb20051f5ee2a4fbcd7ef352806acff8c91e297
Kieran Vella <58534858+kvellaNess@users.noreply.github.com>
2022-04-28 13:28:38 +1000

    Create vms4power.sh

---
+++ vms4power.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Ness VMS4 Power tweak
+#https://github.com/kvellaNess/NxVMS
+wget -O /etc/default/grub https://raw.githubusercontent.com/kvellaNess/NxVMS/master/vms4/etc/default/grub
+# update-grub
