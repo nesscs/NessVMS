23234c7834ad5f1d3c5fb660e90fefc687bdd60e
Kieran Vella <58534858+kvellaNess@users.noreply.github.com>
2022-04-28 14:05:07 +1000

    Update vms4power.sh

--- vms4power.sh
+++ vms4power.sh
@@ -8,5 +8,41 @@ wget https://raw.githubusercontent.com/kvellaNess/NxVMS/master/vms4/etc/default/
 sudo cp /etc/default/grub /etc/default/grub.old
 #Replace with new file
 sudo mv ~/Downloads/grub /etc/default/grub
-
-# update-grub
+#Update Grub file with new command line
+sudo update-grub
+#Finished!
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mAll Done! Please Reboot\e[0m"
+#Flash!
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
