91c4db84eb9afae03734c62cf5ddfe2d685b0bc8
Kieran Vella <58534858+kvellaNess@users.noreply.github.com>
2022-04-28 13:59:07 +1000

    Update vms4power.sh

--- vms4power.sh
+++ vms4power.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #Ness VMS4 Power tweak
 #https://github.com/kvellaNess/NxVMS
+#Download updated file
 cd ~/Downloads
 wget https://raw.githubusercontent.com/kvellaNess/NxVMS/master/vms4/etc/default/grub
-sudo cp ~/Downloads/grub /etc/default/grub
+#Backup old file
+sudo cp /etc/default/grub /etc/default/grub.old
+#Replace with new file
+sudo mv ~/Downloads/grub /etc/default/grub
 
 # update-grub
