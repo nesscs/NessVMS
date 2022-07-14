9c29f4d451a237d429a54bd95dc0ce259942532c
Kieran Vella <kizonline@the-vellas.com>
2022-07-14 12:26:21 +1000

    Update uninstallnxwitness.sh

--- uninstallnxwitness.sh
+++ uninstallnxwitness.sh
@@ -2,6 +2,42 @@
 #Ness VMS Server uninstall Script
 #https://github.com/nesscs/NxVMS
 #This script is unsupported, do not blindly run it
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+#Flash!
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 0.5
+printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 
 #Stop Nx Witness
 sudo service networkoptix-mediaserver stop
