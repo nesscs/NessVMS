592b17c211d0f2d89e1124fa9cd7733df743d3c7
Kieran Vella <kizonline@the-vellas.com>
2022-07-14 12:30:07 +1000

    Update uninstallnxwitness.sh

--- uninstallnxwitness.sh
+++ uninstallnxwitness.sh
@@ -8,36 +8,120 @@ echo ""
 echo ""
 echo ""
 echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m10\e[0m"
 #Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m09\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m08\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m07\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m06\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m05\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m04\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m03\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m02\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m01\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
 sleep 0.5
+
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e "\e[7mYou are about to wipe NxWitness, CTRL - C to cancel\e[0m"
+echo -e "\e[7m00\e[0m"
+#Flash!
 printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
-sleep 0.5
-printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
-sleep 0.5
-printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
-sleep 0.5
-printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
-sleep 0.5
-printf "\x1b[?5h"; sleep .1; printf "\x1b[?5l"
+sleep 1.0
 
 #Stop Nx Witness
 sudo service networkoptix-mediaserver stop
